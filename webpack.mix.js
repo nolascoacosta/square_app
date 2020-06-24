496dfa953756b7480028b0f43b5b273df11b12fa
nolascoacosta <ramonantonionolascoacosta@gmail.com>
2020-06-24 16:57:19 -0400

    setting up frontend using quasar framework

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.copy('frontend/dist/spa/index.html', 'resources/views/app.blade.php')
+    .copyDirectory('frontend/dist/spa', 'public');
